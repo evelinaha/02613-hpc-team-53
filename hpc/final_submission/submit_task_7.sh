64f3e4273c9faea189d0a58fdf25d590ab8d5594
Mads Rehof <53825150+madsrehof@users.noreply.github.com>
2026-04-27 11:54:54 +0200

    Add files via upload

---
+++ hpc/final_submission/submit_task_7.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#BSUB -J Task_7
+#BSUB -q hpc
+#BSUB -R "rusage[mem=1GB]"
+#BSUB -B
+#BSUB -N
+##BSUB -u mekre@dtu.dk
+#BSUB -o outputs/Output_%J.out
+#BSUB -e outputs/Output_%J.err
+#BSUB -W 60
+#BSUB -n 1
+#BSUB -R "span[hosts=1]"
+#BSUB -R "select[model==XeonE5_2650v4]"
+
+source /dtu/projects/02613_2025/conda/conda_init.sh
+conda activate 02613
+
+python3 simulate_jit.py 100 
+
+
+
